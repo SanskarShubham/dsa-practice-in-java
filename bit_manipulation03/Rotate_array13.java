92b38f130c91978efee0d938ff94f81a9d613a8c
shubham ojha <shubhamkumarojha9852@gmail.com>
2024-03-17 14:01:07 +0530

    practice one question rotate array

--- bit_manipulation03/Rotate_array13.java
+++ bit_manipulation03/Rotate_array13.java
@@ -1,25 +1,25 @@
-class Rotate_array {
+import java.util.Arrays;
 
+class Rotate_array {
     public static void rotate(int[] arr, int k) {
-        k = k % arr.length;
-        for (int j = 0; j < k; j++) {
-            int val = arr[0];
-            for (int i = 1; i < arr.length; i++) {
-                int temp = val;
-                val = arr[i];
-                arr[i] = temp;
+        // brute force method
+        int rotate = k % arr.length;
+        for (int i = 0; i < rotate; i++) {
+            int val = arr[i];
+            for (int j = 1; j < arr.length; j++) {
+                int temp = arr[j];
+                arr[j] = val;
+                val = temp;
             }
             arr[0] = val;
         }
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]+" ");
-            
-        }
+
+        System.out.println(Arrays.toString(arr));
     }
 
     public static void main(String[] args) {
-            int[] arr  = {1,2,3,4,5};
-            int k =6;
-            rotate(arr,k);
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int k = 6;
+        rotate(arr, k);
     }
 }
